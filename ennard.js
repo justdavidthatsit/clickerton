8300ce73c34cc60467802e676e018db97905b923
dave <justdavidthatsall@gmail.com>
2022-03-24 14:20:43 -0500

    grreat
    f

---
+++ ennard.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+  const t = document.querySelector('.transition');
+  const a = document.querySelectorAll('a');
+
+  setTimeout(() => {
+    t.classList.remove('is-active');
+  }, 600)
+
+  for (let i=0; i<a.length; i++){
+    const as=a[i];
+
+    as.addEventListener('click', e=>{
+      e.preventDefault();
+      let target = e.target.href;
+
+      t.classList.add('is-active');
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 400);
+    });
+  }
+}
